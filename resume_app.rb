c7e200e816a8b05a6d8b8203b1e0c0841b895504
Will C. Fisher, Jr <willcfisherjr@gmail.com>
2014-10-15 16:59:19 -0400

    still trying

--- resume_app.rb
+++ resume_app.rb
@@ -13,19 +13,3 @@ end
 get '/resume' do
   haml :resume
 end
-
-# get '/faker_test' do
-#   @wisdom = Faker::Hacker.say_something_smart
-#   erb :faker_test
-# end
-
-# get '/fake_resume' do
-#   @name = Faker::Name.name
-#   @email = Faker::Internet.free_email
-#   @phone = Faker::PhoneNumber.cell_phone
-#   erb :fake_resume, layout: :fake_layout
-# end
-
-
-# Inquire about parameter passing in class tomorrow! 
-# This is the one aspect of the homework that escaped me.
